fb08d2bccd7a0900b13c0a3c79ab1f053d41d814
DAvalos36 <davalos@itparral.edu.mx>
2023-11-19 15:11:19 -0600

    feat: primer ejemplo funcional

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,67 +9,110 @@ function App() {
 	const [modelosCargados, setModelosCargados] = useState(false);
 	const [modeloEntrenado, setModeloEntrenado] = useState(false);
 
-	const imgRef = useRef<HTMLImageElement | null>(null);
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const imgRef = useRef<HTMLImageElement>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	async function cargarModelos() {
 		Promise.all([
 			faceapi.loadSsdMobilenetv1Model("/models"),
 			faceapi.loadFaceLandmarkModel("/models"),
 			faceapi.loadFaceRecognitionModel("/models"),
-		]).then(() => setModelosCargados(true));
+		]).then(() => {
+			setModelosCargados(true);
+			return true;
+		});
 	}
 
 	async function entrenarRostros() {
 		const labeledFaceDescriptors = await Promise.all(
-			nC.map(async (label) => {
-				variaciones.map(async (variacion) => {
-					// fetch image data from urls and convert blob to HTMLImage element
-					const imgUrl = `alumnos/${label}_${variacion}.jpeg.jpeg`;
-					const img = await faceapi.fetchImage(imgUrl);
+			nC.map(async (label, i) => {
+				// fetch image data from urls and convert blob to HTMLImage element
+				const imgUrl = `alumnos/${label}_${i + 1}.jpg`;
+				const img = await faceapi.fetchImage(imgUrl);
 
-					// detect the face with the highest score in the image and compute it's landmarks and face descriptor
-					const fullFaceDescription = await faceapi
-						.detectSingleFace(img)
-						.withFaceLandmarks()
-						.withFaceDescriptor();
+				// detect the face with the highest score in the image and compute it's landmarks and face descriptor
+				const fullFaceDescription = await faceapi
+					.detectSingleFace(img)
+					.withFaceLandmarks()
+					.withFaceDescriptor();
 
-					if (!fullFaceDescription) {
-						throw new Error(`no faces detected for ${label}`);
-					}
+				if (!fullFaceDescription) {
+					throw new Error(`no faces detected for ${label}`);
+				}
 
-					const faceDescriptors = [fullFaceDescription.descriptor];
-					return new faceapi.LabeledFaceDescriptors(label, faceDescriptors);
-				});
+				console.log(`${label} ${i + 1}`);
+
+				const faceDescriptors = [fullFaceDescription.descriptor];
+				return new faceapi.LabeledFaceDescriptors(
+					`${label}_${i + 1}`,
+					faceDescriptors,
+				);
 			}),
 		);
-
+		console.log(JSON.stringify(labeledFaceDescriptors));
 		setModeloEntrenado(true);
+		return labeledFaceDescriptors;
 	}
 
-	useEffect(() => {
-		async function a() {
-			await cargarModelos();
-			if (canvasRef && canvasRef.current) {
-				canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(
-					videoRef.current,
-				);
-			}
+	async function a() {
+		if (imgRef?.current && canvasRef && canvasRef.current) {
+			const canvas = canvasRef.current;
+			canvas.width = imgRef.current.width;
+			canvas.height = imgRef.current.height;
+			// if (canvasRef && canvasRef.current) {
+			// 	canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(
+			// 		imgRef.current as HTMLImageElement,
+			// 	);
+			// }
 			const fullFaceDescriptions = await faceapi
 				.detectAllFaces(imgRef.current as HTMLImageElement)
 				.withFaceLandmarks()
 				.withFaceDescriptors();
 			// fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions);
-			// faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
-			// await entrenarRostros();
+			console.log(fullFaceDescriptions);
+			alert(fullFaceDescriptions.length);
+			faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
+			alert("dibujados");
+			const l = await entrenarRostros();
+
+			const maxDescriptorDistance = 0.6;
+			const faceMatcher = new faceapi.FaceMatcher(l, maxDescriptorDistance);
+
+			const results = fullFaceDescriptions.map((fd) =>
+				faceMatcher.findBestMatch(fd.descriptor),
+			);
+
+			results.forEach((bestMatch, i) => {
+				const box = fullFaceDescriptions[i].detection.box;
+				const text = bestMatch.toString();
+				const drawBox = new faceapi.draw.DrawBox(box, { label: text });
+				drawBox.draw(canvas);
+			});
 		}
-		a();
+	}
+
+	useEffect(() => {
+		cargarModelos();
 	}, []);
 
 	return (
 		<div className="min-h-screen flex flex-col justify-center items-center">
-			<img ref={imgRef} src="/prb.jpeg" alt="FOTO" id="imgPrueba" />
-			<canvas ref={canvasRef} style={{ position: "absolute" }} /> <h1>Hola</h1>
+			{modelosCargados && (
+				<img
+					ref={imgRef}
+					src="/prb.jpeg"
+					onLoad={a}
+					alt="FOTO"
+					id="imgPrueba"
+				/>
+			)}
+			<canvas
+				ref={canvasRef}
+				width={600}
+				height={400}
+				style={{ border: "1px solid #000", position: "absolute" }}
+			/>
+			<h1>Hola</h1>
 			<Button color="primary">Prueba</Button>
 			{modelosCargados ? <h1>Modelos cargados</h1> : <h1>Cargando modelos</h1>}
 			{modeloEntrenado ? <h1>Modelo entrenado</h1> : <h1>Entrenando modelo</h1>}
