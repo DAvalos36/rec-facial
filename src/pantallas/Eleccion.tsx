4c2500446db4bbf1b06981d24891eea905d9c509
corazon03 <yleyva@itparral.edu.mx>
2023-11-25 17:32:24 -0600

    UI

--- src/pantallas/Eleccion.tsx
+++ src/pantallas/Eleccion.tsx
@@ -70,7 +70,7 @@ const App: React.FC = () => {
         <div className='min-h-screen flex justify-center self-center bg-cyan-500/[0.7]'>
             <div className='max-w-md py-2 flex justify-center self-center flex-col'>
                 <ImageSlider images={images} interval={5000} /> {/* Cambiar el intervalo según sea necesario */}
-                <div className='py-2 flex justify-around'>
+                <div className='py-4 flex justify-around'>
                     <Button className='' onPress={openCamera} endContent={<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="#000" d="M4 4h3l2-2h6l2 2h3a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2m8 3a5 5 0 0 0-5 5a5 5 0 0 0 5 5a5 5 0 0 0 5-5a5 5 0 0 0-5-5m0 2a3 3 0 0 1 3 3a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3Z" /></svg>}>Capturar imagen</Button>
                     <Modal isOpen={isOpen} onOpenChange={closeCamera}>
                         <ModalContent>
