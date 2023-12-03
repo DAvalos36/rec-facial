b004dd236a10b38ba3176863f51f8768579a97f8
DAvalos36 <davalos@itparral.edu.mx>
2023-12-02 23:00:47 -0600

    feat: hash routes implementadas

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,32 @@ import EntrenarModelo from "components/EntrenarModelo";
 import Principal from "pantallas/Principal";
 import Eleccion from "pantallas/Eleccion";
 import Inicio from "pantallas/Inicio";
-import { Route } from "wouter";
+import { Route, Router as R } from "wouter";
+import {
+	BaseLocationHook,
+	navigate,
+	useLocationProperty,
+} from "wouter/use-location";
+
+const hashLocation = () => window.location.hash.replace(/^#/, "") || "/";
+
+const hashNavigate = (to: string) => navigate(`#${to}`);
+
+const useHashLocation: BaseLocationHook = () => {
+	const location = useLocationProperty(hashLocation);
+	return [location, hashNavigate];
+};
 
 const Router = () => (
 	<>
-		<Route path="/" children={<Inicio />} />
-		<Route path="/eleccion" children={<Eleccion />} />
-		<Route path="/foto" children={<Principal />} />
-		{/* <Route path="/webcam" children={<Webcamm />} /> */}
+		<R hook={useHashLocation}>
+			<Route path="/" children={<Inicio />} />
+			<Route path="/eleccion" children={<Eleccion />} />
+			<Route path="/foto" children={<Principal />} />
+			{/* <Route path="/webcam" children={<Webcamm />} /> */}
 
-		<Route path="/entrenar" children={<EntrenarModelo />} />
+			<Route path="/entrenar" children={<EntrenarModelo />} />
+		</R>
 	</>
 );
 
