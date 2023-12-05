0c699e638f7091f97c4df29d9ea8d59ff9ed6e02
KennethDom <102487889+KennethDom@users.noreply.github.com>
2023-12-04 19:52:13 -0600

    responsivo

--- src/components/PantallaCarga.tsx
+++ src/components/PantallaCarga.tsx
@@ -4,7 +4,7 @@ import Random from 'Random'
 import { Player } from '@lottiefiles/react-lottie-player';
 
 function PantallaCarga() {
-	return <div className="bg-customYellow ">
+	return <div className="bg-customYellow max-w-screen-md mx-auto ">
 		<Random />
 		<Player
 			src='https://lottie.host/b480ea22-a680-4b0f-946e-2a0d8036a0a8/XUmuecKqCz.json'
